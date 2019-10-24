fbeca0fdcfb979a6ac06bc0275b3d5c257064310
r.h <zoho1381@gmail.com>
2019-10-24 11:56:54 +0330

    change toolbar title and visibility for setting fragment

--- app/src/main/java/com/rhosseini/navigationsample/multiNavHost/listScreen/SettingFragment.kt
+++ app/src/main/java/com/rhosseini/navigationsample/multiNavHost/listScreen/SettingFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.Toolbar
 
 import com.rhosseini.navigationsample.R
 
@@ -20,6 +21,14 @@ class SettingFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        val toolbar = activity?.findViewById<Toolbar>(R.id.toolbar)
+        toolbar?.title = "custom title"
+        toolbar?.visibility = View.GONE
+
+        super.onActivityCreated(savedInstanceState)
     }
 
 }
